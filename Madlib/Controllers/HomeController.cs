c4f166b665185b5f45e3e43552d0a627b629d976
Sploob <crayiphone@gmail.com>
2022-05-09 14:53:48 -0700

    add space story
    Co-authored-by: Marcus Anthony Lorenzo <marcusanthonylorenzo@users.noreply.github.com>

--- Madlib/Controllers/HomeController.cs
+++ Madlib/Controllers/HomeController.cs
@@ -13,18 +13,6 @@ namespace Madlib.Controllers
       return View();
     }
 
-    // [Route("/madlibform")]
-    // public ActionResult MadlibForm()
-    // {
-    //   return View();
-    // }
-
-    [Route("/madlibformtwo")]
-    public ActionResult MadlibFormTwo()
-    {
-      return View();
-    }
-
     [Route("/random")]
     public ActionResult MadlibForm(string form)
     {
@@ -32,7 +20,7 @@ namespace Madlib.Controllers
       {
         MadlibVariable myMadlibVariable = new MadlibVariable();
         Random rand = new Random();
-        int randomView = rand.Next(2);
+        int randomView = rand.Next(1, 4);
         myMadlibVariable.Random = randomView;
         return View(myMadlibVariable);
       }
@@ -48,11 +36,18 @@ namespace Madlib.Controllers
         myMadlibVariable.Random = 2;
         return View(myMadlibVariable);
       }
+      else if (form == "space")
+      {
+        MadlibVariable myMadlibVariable = new MadlibVariable();
+        myMadlibVariable.Random = 3;
+        return View(myMadlibVariable);
+      }
       else
       {
         return View();
       }
     }
+
     [Route("/madlibgame")]
     public ActionResult MadlibGame(string noun, string adjective, string exclamation, string verb)
     {
@@ -62,6 +57,38 @@ namespace Madlib.Controllers
       myMadlibVariable.Adjective = adjective;
       myMadlibVariable.Exclamation = exclamation;
       myMadlibVariable.Verb = verb;
+      return View(myMadlibVariable);
+    }
+
+    [Route("/madlibgametwo")]
+    public ActionResult MadlibGameTwo(string noun, string nounTwo, string adjective, string exclamation, string verb, string verbTwo, string verbThree)
+    {
+      MadlibVariable myMadlibVariable = new MadlibVariable();
+      
+      myMadlibVariable.Noun = noun;
+      myMadlibVariable.Noun2 = nounTwo;
+      myMadlibVariable.Adjective = adjective;
+      myMadlibVariable.Exclamation = exclamation;
+      myMadlibVariable.Verb = verb;
+      myMadlibVariable.Verb2 = verbTwo;
+      myMadlibVariable.Verb3 = verbThree;
+      return View(myMadlibVariable);
+    }
+
+    [Route("/madlibgamethree")]
+    public ActionResult MadlibGameThree(string noun, string nounTwo, string adjective, string adjectiveTwo, string exclamation, string exclamationTwo, string verb, string verbTwo, string verbThree)
+    {
+      MadlibVariable myMadlibVariable = new MadlibVariable();
+      
+      myMadlibVariable.Noun = noun;
+      myMadlibVariable.Noun2 = nounTwo;
+      myMadlibVariable.Adjective = adjective;
+      myMadlibVariable.Adjective2 = adjectiveTwo;
+      myMadlibVariable.Exclamation = exclamation;
+      myMadlibVariable.Exclamation2 = exclamationTwo;
+      myMadlibVariable.Verb = verb;
+      myMadlibVariable.Verb2 = verbTwo;
+      myMadlibVariable.Verb3 = verbThree;
       return View(myMadlibVariable);
     }
 
--- Madlib/Models/MadlibVariable.cs
+++ Madlib/Models/MadlibVariable.cs
@@ -5,7 +5,9 @@ namespace Madlib.Models
     public string Noun { get; set; }
     public string Noun2 { get; set; }
     public string Adjective { get; set; }
+    public string Adjective2 { get; set; }
     public string Exclamation { get; set; }
+    public string Exclamation2 { get; set; }
     public string Verb { get; set; }
     public string Verb2 { get; set; }
     public string Verb3 { get; set; }
